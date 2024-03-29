b310063dbadba8918bc3432e161b886546b276ef
Jader <swilor29@gmail.com>
2024-03-29 16:12:10 -0500

    refactor: :recycle: limpieza de codigo

--- src/store/movie.store.ts
+++ src/store/movie.store.ts
@@ -4,23 +4,10 @@ import { Movie } from "../interface/movie.interface";
 interface MovieState {
   movies: Movie[];
   setMovies: (movies: Movie[]) => void;
-/*   setMoviesStorage: (id: number) => void;
-  getMoviesLocalStorage: () => void;
-  moviesLocal:Movie[] | null */
 }
 
 export const useMovieStore = create<MovieState>()((set) => ({
   movies: [],
-  moviesLocal:[],
+  moviesLocal: [],
   setMovies: (movies: Movie[]) => set({ movies }),
-/*   setMoviesStorage: (id: number) => {
-    const moviesIds = [];
-    moviesIds.push({ id: id });
-    localStorage.setItem("@moviesDb@", JSON.stringify(moviesIds));
-  },
-  getMoviesLocalStorage: () => {
-    const moviesJSON = localStorage.getItem("@moviesDb@");
-    const movies = moviesJSON ? (JSON.parse(moviesJSON) as Movie[]) : null;
-    get().moviesLocal = movies
-  }, */
 }));
